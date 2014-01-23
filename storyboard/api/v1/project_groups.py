d1a28caaa5a72b5e001dbcc18e3166003fe953ae
Michael Krotscheck <krotscheck@gmail.com>
2014-01-22 17:55:43 -0800

    Update ProjectGroups API to consume ID's rather than names.
    While using names in our API is nice and human-readable, it's actually
    much better to use a unique identifier (in this case, the database ID).
    This avoids situations in which records become inaccessible because their
    names are identical. Say record 3 has a name of "Test Project Group", and
    record 4 is then changed to be called "Test Project Group", suddenly one of
    these two records is no longer visible via
      GET /v1/project_groups/Test%20Project%20Group
    Also, it means that we don't have to worry about wacky UTF-8 characters
    encoding in the URL breaking our browsers.
    
    Also, for the sake of remaining RESTful, I removed the nested association
    with projects. That should probably be exposed at /v1/project_groups/1/projects/
    instead.
    
    Change-Id: I1fc5308655cc96a111181275316e9fb574523a4e

--- storyboard/api/v1/project_groups.py
+++ storyboard/api/v1/project_groups.py
@@ -22,11 +22,11 @@ import storyboard.api.v1.wsme_models as wsme_models
 
 class ProjectGroupsController(rest.RestController):
 
-    @wsme_pecan.wsexpose(wsme_models.ProjectGroup, unicode)
-    def get_one(self, name):
-        group = wsme_models.ProjectGroup.get(name=name)
+    @wsme_pecan.wsexpose(wsme_models.ProjectGroup, int)
+    def get_one(self, id):
+        group = wsme_models.ProjectGroup.get(id=id)
         if not group:
-            raise ClientSideError("Project Group %s not found" % name,
+            raise ClientSideError("Project Group %s not found" % id,
                                   status_code=404)
         return group
 
@@ -34,3 +34,19 @@ class ProjectGroupsController(rest.RestController):
     def get(self):
         groups = wsme_models.ProjectGroup.get_all()
         return groups
+
+    @wsme_pecan.wsexpose(wsme_models.ProjectGroup,
+                         body=wsme_models.ProjectGroup)
+    def post(self, group):
+        created_group = wsme_models.ProjectGroup.create(wsme_entry=group)
+        if not created_group:
+            raise ClientSideError("Could not create ProjectGroup")
+        return created_group
+
+    @wsme_pecan.wsexpose(wsme_models.ProjectGroup, int,
+                         body=wsme_models.ProjectGroup)
+    def put(self, id, group):
+        updated_group = wsme_models.ProjectGroup.update("id", id, group)
+        if not updated_group:
+            raise ClientSideError("Could not update group %s" % id)
+        return updated_group
