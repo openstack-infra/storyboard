903444fbdf5f47b3402d9a8336fe3726bf7ee68b
Adam Coldrick <adam.coldrick@codethink.co.uk>
2015-08-17 14:30:06 +0000

    Set correct default credentials in the test_backend.sh scripts
    The CI expects user/pass to be openstack_citest/openstack_citest,
    but it currently defaults to storyboard/storyboard in the
    {install,destroy}_test_backend.sh scripts.
    
    Change-Id: Icbf912dd304286638f31ae99a29c99905a423e90

--- tools/destroy_test_backend.sh
+++ tools/destroy_test_backend.sh
@@ -5,8 +5,8 @@ echo 'Killing storyboard-api...'
 killall storyboard-api
 
 # export default vars if not defined, to be available in conf file
-export DB_USER=${DB_USER:-storyboard}
-export DB_PASSWORD=${DB_PASSWORD:-storyboard}
+export DB_USER=${DB_USER:-openstack_citest}
+export DB_PASSWORD=${DB_PASSWORD:-openstack_citest}
 export DB_TEST=${DB_TEST:-storyboard_test}
 
 mysql -u $DB_USER -p$DB_PASSWORD -e "DROP DATABASE IF EXISTS $DB_TEST;"
